e5bdf24823d1d6a4f4caf15a2370ded01b293dea
Edward <espadadeoro1o@gmail.com>
2026-05-15 15:24:18 -0400

    Limpieza de proyecto: Eliminación de archivos temporales y scripts de utilidad

--- check_users.js
+++
@@ -1,8 +0,0 @@
-const pool = require('./backend/src/config/db');
-pool.query('SELECT id_usuario, cedula, rol, nombre FROM "Usuarios"')
-  .then(r => {
-    console.log('--- LISTA DE USUARIOS ---');
-    console.table(r.rows);
-  })
-  .catch(e => console.error(e))
-  .finally(() => process.exit());
--- fix_password.js
+++
@@ -1,6 +0,0 @@
-const pool = require('./backend/src/config/db');
-const newHash = '$2b$10$AIxEp06G/dV4ILsmiL6jt.J82XoTVmKS/OqiO9y/RMKpKEDssdq1y';
-pool.query('UPDATE "Usuarios" SET password_hash = $1 WHERE cedula = $2', [newHash, '12345'])
-  .then(() => console.log('CONTRASENA DE MEDICO ACTUALIZADA A: 123456'))
-  .catch(e => console.error(e))
-  .finally(() => process.exit());
