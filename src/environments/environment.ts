0fb4e345ac368601ec9cdf4dcee552c8e09bdd13
Edward <espadadeoro1o@gmail.com>
2026-07-21 12:19:16 -0400

    arreglo de enviroment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,6 @@
-// src/environments/environment.ts
 export const environment = {
   production: false,
-  apiUrl: 'https://api-cola-cat.clinicanuevacaracas.net/api',
-  socketUrl: 'https://api-cola-cat.clinicanuevacaracas.net',
-  socketTransports: ['polling'] as const,
+  apiUrl: '/api',
+  socketUrl: '/',
+  socketTransports: ['polling', 'websocket'] as const,
 };
