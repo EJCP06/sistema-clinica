af9278a9b0bb66ff74d98c4e38f63430b585ec59
Edward <espadadeoro1o@gmail.com>
2026-07-23 07:34:02 -0400

    cambios en el api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@
 export const environment = {
   production: true,
   apiUrl: '/api',
-  socketUrl: 'https://api-cola-cat.clinicanuevacaracas.net',
+  socketUrl: '/',
   socketTransports: ['polling'] as const,
 };
